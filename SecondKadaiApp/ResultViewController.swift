aa346ff6118912848f5cfcd6c87badf4286e1cfc
鈴木貴広 <takahiro.suzuki@gmail.com>
2017-01-12 12:40:20 +0900

    課題完成 conflicting constraintsの警告内容を要理解（重複の意味は理解済み。警告文の読み方とどのような手順にてAutoLayoutすると良いのかを確認）

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Takahiro Suzuki on 2017/01/09.
+//  Copyright © 2017年 Takahiro Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    var name: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        label.text = "こんにちは、\(name)さん"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textfield: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.name = textfield.text!
+        
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+    }
+    
 }
+
 
